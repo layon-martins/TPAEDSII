4d2d01bcc950cd54f79abb5d5571a7541b4c1a06
Ana Carolina <ana.clementino@ufv.br>
2023-05-17 21:19:20 -0300

    adicionando tratamentoDePalavras funcionando

--- Tratamento de Arquivos/TratamentoDeArquivos.c
+++ Tratamento de Arquivos/TratamentoDeArquivos.c
@@ -3,41 +3,42 @@
 #include <string.h>
 #include <ctype.h>
 
+void tratamentoDePalavras(char *palavra);
+
 int main(){
     FILE *arquivo = fopen("arquivo1.txt", "r");
     char palavra[100];
     char *ptr_palavra;
 
     if(arquivo == NULL){
-		printf("Nao foi possivel abrir o arquivo\n");
-		return 0;
-	}
+		  printf("Nao foi possivel abrir o arquivo\n");
+		  return 0;
+	  }
 
-    while(!feof(arquivo)){
-        fscanf(arquivo, "%s", palavra);   
-        strcpy(palavra, tolower(&palavra));     
+    while(!feof(arquivo)){   
+        fscanf(arquivo, "%s", palavra); 
+        tratamentoDePalavras(palavra);
+        //Quando existem dois caracteres seguidos ele guarda um espaco em branco
         printf("%s\n", palavra);
     }
-
-
-    
-
     fclose(arquivo);
 
     return 0;
 
 }
 
-// transforma para minúsculo e remove caracteres especiais.
-void TransformarParaMinusculo(char *palavra){
-  int i;
-  int ascii;
+void tratamentoDePalavras(char *palavra){
+  int i;  
+  int ascii;  
   for (i = 0; i < strlen(palavra); i++){
+    // tolower funciona em caracter
     palavra[i] = tolower(palavra[i]);
 
+    //transforma a palavra para um valor na tabela ascii
     ascii = (int)palavra[i];
-
-    if (ascii < 97 || ascii > 122)
+    if (ascii < 97 || ascii > 122){
+      //caso o caracter nao esteja entre a e z minusculos, ele coloca como fim de string
       palavra[i] = '\0';
+    }
   }
 }
