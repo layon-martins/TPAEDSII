fe563f3ca78442331fc7fd3217d5235519e91c87
bahpags <barbarapagnocca@gmail.com>
2023-05-18 09:36:41 -0300

    Mudança na entrada e comentário no main

--- Tratamento de Arquivos/TratamentoDeArquivos.c
+++ Tratamento de Arquivos/TratamentoDeArquivos.c
@@ -1,44 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <string.h>
+  #include <ctype.h> // para usar a função tolower
 
-void tratamentoDePalavras(char *palavra);
+  void tratamentoDePalavras(char *palavra);
 
-int main(){
-    FILE *arquivo = fopen("arquivo1.txt", "r");
-    char palavra[50];
-    char *ptr_palavra;
+  int main(){
+      FILE *arquivo = fopen("entrada.txt", "r");
+      char palavra[50];
+      char *ptr_palavra;
 
-    if(arquivo == NULL){
-		  printf("Não foi possivel abrir o arquivo\n");
-		  return 0;
-	  }
+      if(arquivo == NULL){
+        printf("Não foi possivel abrir o arquivo\n");
+        return 0;
+      }
 
-    while(!feof(arquivo)){   
-        fscanf(arquivo, "%s", palavra); 
-        tratamentoDePalavras(palavra);
-        //Quando existem dois caracteres seguidos ele guarda um espaco em branco
-        printf("%s\n", palavra);
-    }
-    fclose(arquivo);
+      while(!feof(arquivo)){   
+          fscanf(arquivo, "%s", palavra); 
+          tratamentoDePalavras(palavra);
+          //Quando existem dois caracteres seguidos ele guarda um espaco em branco
+          printf("%s\n", palavra);
+      }
+      fclose(arquivo);
 
-    return 0;
+      return 0;
 
-}
+  }
 
-void tratamentoDePalavras(char *palavra){
-  int i;  
-  int ascii;  
-  for (i = 0; i < strlen(palavra); i++){
-    // tolower funciona em caractere
-    palavra[i] = tolower(palavra[i]);
+  void tratamentoDePalavras(char *palavra){
+    int i;  
+    int ascii;  
+    for (i = 0; i < strlen(palavra); i++){
+      // tolower funciona em caractere
+      palavra[i] = tolower(palavra[i]);
 
-    //transforma a palavra para um valor na tabela ascii
-    ascii = (int)palavra[i];
-    if (ascii < 97 || ascii > 122){
-      //caso o caracter não esteja entre a e z minúsculos, ele coloca como fim de string
-      palavra[i] = '\0';
+      //transforma a palavra para um valor na tabela ascii
+      ascii = (int)palavra[i];
+      if (ascii < 97 || ascii > 122){
+        //caso o caracter não esteja entre a e z minúsculos, ele coloca como fim de string
+        palavra[i] = '\0';
+      }
     }
   }
-}
