276516545a324c09fb03733ab3e3f477db5ccb83
JessCris <98407222+JessCris@users.noreply.github.com>
2023-05-27 12:48:55 -0300

    Update TratamentoDeArquivos.c
    Cabeçalho

--- Tratamento de Arquivos/TratamentoDeArquivos.c
+++ Tratamento de Arquivos/TratamentoDeArquivos.c
@@ -1,3 +1,11 @@
+/* Trabalho Prático 1 - Disciplina AEDS 2 - UFV CAF
+ * Integrantes:
+ * Ana Carolina Fernandes - 5094
+ * Bárbara Pagnocca - 5061
+ * Jéssica Cristina Carvalho - 4686
+ * Layon Fonseca Martins - 4220
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
