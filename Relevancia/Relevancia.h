46aafea71ecaabe311541563eeef1d9a1aae356e
bahpags <barbarapagnocca@gmail.com>
2023-05-29 16:15:27 -0300

    relevância.h

--- Relevancia/Relevancia.h
+++ Relevancia/Relevancia.h
@@ -1,1 +1,6 @@
 #include "../Patricia/Patricia.h"
+
+// calcula a relevância de cada arquivo
+void relevancia(int *vRelevancia, Aptd_Pat *A, int N, String *termos, int quantT);
+
+float peso(int arq, Aptd_Pat A, int N, String *termos, int quantT);
