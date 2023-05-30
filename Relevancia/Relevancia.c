e88944bd49e8522eff8506992f4d2a259da2e2bd
Ana Carolina <ana.clementino@ufv.br>
2023-05-29 23:52:37 -0300

    Relevancia

--- Relevancia/Relevancia.c
+++ Relevancia/Relevancia.c
@@ -1,17 +1,3 @@
-
-/* Trabalho Prático 1 - Disciplina AEDS 2 - UFV CAF
- * Integrantes:
- * Ana Carolina Fernandes - 5094
- * Bárbara Pagnocca - 5061
- * Jéssica Cristina Carvalho - 4686
- * Layon Fonseca Martins - 4220
-*/
-
-
-
-#include "Relevancia.h"
-
-
 #include "relevancia.h"
 
 void relevancia(float *vRelevancia, Aptd_Pat *A, int N, String *termos, int quantT){
